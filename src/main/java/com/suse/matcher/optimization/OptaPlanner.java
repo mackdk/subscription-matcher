b51aa97ad4afc7a8e7231d2f30b84293c66f42a5
Thomas Florio <thomas.florio@suse.com>
2024-06-04 16:53:10 +0200

    Updated Drools/OptaPlanner to the last 7.x version

--- src/main/java/com/suse/matcher/optimization/OptaPlanner.java
+++ src/main/java/com/suse/matcher/optimization/OptaPlanner.java
@@ -11,9 +11,8 @@ import org.optaplanner.core.config.localsearch.LocalSearchPhaseConfig;
 import org.optaplanner.core.config.phase.PhaseConfig;
 import org.optaplanner.core.config.solver.EnvironmentMode;
 import org.optaplanner.core.config.solver.SolverConfig;
-import org.optaplanner.core.impl.score.director.drools.DroolsScoreDirector;
+import org.optaplanner.core.impl.score.director.ScoreDirectorFactory;
 import org.optaplanner.core.impl.score.director.drools.DroolsScoreDirectorFactory;
-import org.optaplanner.core.impl.solver.DefaultSolver;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -47,14 +46,16 @@ public class OptaPlanner {
         }
 
         // init solver
-        Solver<Assignment> solver = initSolver(testing);
+        SolverConfig configuration = createConfiguration(testing);
+        SolverFactory<Assignment> solverFactory = SolverFactory.create(configuration);
+        Solver<Assignment> solver = solverFactory.buildSolver();
 
         // solve problem
         long start = System.currentTimeMillis();
-        solver.solve(unsolved);
+        result = solver.solve(unsolved);
         LOGGER.info("Optimization phase took {}ms", System.currentTimeMillis() - start);
-        result = solver.getBestSolution();
         LOGGER.info("{} matches confirmed", result.getMatches().stream().filter(m -> m.isConfirmed()).count());
+
         if (LOGGER.isDebugEnabled()) {
             // Log confirmed matches
             result.getMatches().stream()
@@ -63,22 +64,19 @@ public class OptaPlanner {
 
             // Show the Penalty facts generated in Scores.drl using DroolsScoreDirector and re-calculating the score
             // of the best solution, because facts generated dynamically are not available outside of this object
-            logOneTwoPenalties(solver, result);
+            logOneTwoPenalties(solverFactory.getScoreDirectorFactory(), configuration.getEnvironmentMode(), result);
         }
     }
 
-    private static void logOneTwoPenalties(Solver<Assignment> solver, Assignment result) {
+    private static void logOneTwoPenalties(ScoreDirectorFactory<Assignment> scoreDirectorFactory,
+                                           EnvironmentMode environmentMode, Assignment result) {
         // Make sure the runtime instances are of the correct types
-        if (!(solver instanceof DefaultSolver) || !(solver.getScoreDirectorFactory() instanceof DroolsScoreDirectorFactory)) {
+        if (!(scoreDirectorFactory instanceof DroolsScoreDirectorFactory)) {
             return;
         }
 
-        DefaultSolver<Assignment> defaultSolver = (DefaultSolver<Assignment>) solver;
-        DroolsScoreDirectorFactory<Assignment> directorFactory = (DroolsScoreDirectorFactory<Assignment>) defaultSolver.getScoreDirectorFactory();
-        EnvironmentMode environmentMode = defaultSolver.getEnvironmentMode();
-
         // Build a new score director and re-evaluate the score
-        try (DroolsScoreDirector<Assignment> director = directorFactory.buildScoreDirector(true, environmentMode.isAsserted())) {
+        try (var director = ((DroolsScoreDirectorFactory<Assignment>) scoreDirectorFactory).buildScoreDirector(true, environmentMode.isAsserted())) {
             director.setWorkingSolution(director.cloneSolution(result));
             director.calculateScore();
 
@@ -101,17 +99,16 @@ public class OptaPlanner {
      * @return the solver
      * @param testing true if running as a unit test, false otherwise
      */
-    private Solver<Assignment> initSolver(boolean testing) {
+    private SolverConfig createConfiguration(boolean testing) {
         try (InputStream stream = OptaPlanner.class.getResourceAsStream("solver-config.xml")) {
             if (stream == null) {
                 throw new IllegalStateException("Unable to locate planner configuration");
             }
 
-            SolverFactory<Assignment> factory = SolverFactory.createFromXmlInputStream(stream);
+            SolverConfig config = SolverConfig.createFromXmlInputStream(stream);
 
             // Tweak parameters in unit tests, which deal with fewer data and need to run faster.
             if (testing) {
-                SolverConfig config = factory.getSolverConfig();
                 // Activate OptaPlanner full assertions to catch more issues
                 config.setEnvironmentMode(EnvironmentMode.FULL_ASSERT);
                 // Reduce the number of steps we accept with no improvement during the local search phase
@@ -121,7 +118,7 @@ public class OptaPlanner {
                     .forEach(terminationConfig -> terminationConfig.setUnimprovedStepCountLimit(12));
             }
 
-            return factory.buildSolver();
+            return config;
         }
         catch (IOException ex) {
             throw new IllegalStateException("Unable to parse planner configuration", ex);
